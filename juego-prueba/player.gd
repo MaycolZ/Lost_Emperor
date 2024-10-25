3dfbf361b2adc75e85da51ed7bbb2113239eb271
Maycol Zincker <zincker2000@gmail.com>
2024-10-25 01:35:29 -0300

    Creacion de movimientos para el personaje

--- juego-prueba/player.gd
+++ juego-prueba/player.gd
@@ -2,29 +2,61 @@ extends CharacterBody2D
 
 
 const SPEED = 100.0
-const JUMP_VELOCITY = -500.0
+const JUMP_VELOCITY = -300.0
+const gravity = 900
 
+const DASH_SPEED = 800
+var dashing = false
+var can_dash = true
 
 func _physics_process(delta):
-	# Add the gravity.
+	
+	# Gravedad
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		if velocity.y > 0:
+			$AnimationPlayer.play("fall")
+		else:
+			$AnimationPlayer.play("jump")
 
-	# Handle jump.
-	if Input.is_action_pressed("ui_accept") and is_on_floor():
+	# Saltar
+	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	if velocity.x >= 0:
-		$Sprite2D.flip_h = false
-	else:
-		$Sprite2D.flip_h = true
+		
+	if Input.is_action_just_pressed("dash") and can_dash:
+		dashing = true
+		can_dash = false
+		$dash_timer.start()
+		$dash_again_timer.start()
+		$AnimationPlayer.play("dash")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("left", "right")
 	if direction:
-		velocity.x = direction * SPEED
+		if dashing:
+			velocity.x = direction * DASH_SPEED
+		else:
+			velocity.x = direction * SPEED
+		if is_on_floor():
+			$AnimationPlayer.play("run")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if is_on_floor():
+			$AnimationPlayer.play("idle")
+			
+	# Rotacion
+	if direction == 1:
+		$Sprite2D.flip_h = false
+	elif direction == -1:
+		$Sprite2D.flip_h = true
 
 	move_and_slide()
+
+# Parar el dash
+func _on_dash_timer_timeout():
+	dashing = false
+
+
+func _on_dash_again_timer_timeout():
+	can_dash = true
